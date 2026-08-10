1eb136b50e4b3423a5502c84042c892cc0c54ecc
katherine711 <sejlyn2006@gmail.com>
2026-08-10 18:36:51 +0530

    Add: find-in-mountain-array - submission-2

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-2.java
@@ -0,0 +1,64 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int n=mountainArr.length();
+
+        int l=0, r=n-1;
+        int peak=0;
+
+        while(l<r){
+            int mid=l+(r-l)/2;
+
+            if(mountainArr.get(mid)<mountainArr.get(mid+1)){
+                l=mid+1;
+            }
+            else{
+                r=mid;
+            }
+        }
+
+        peak=l;
+
+        l=0;
+        r=peak;
+
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(target==mountainArr.get(mid)){
+                return mid;
+            }
+            else if(target<mountainArr.get(mid)){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+
+        l=peak;
+        r=n-1;
+
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(target==mountainArr.get(mid)){
+                return mid;
+            }
+            else if(target>mountainArr.get(mid)){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+
+        return -1;
+    }
+}
